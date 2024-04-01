693a1b47e1761b6b3c24b6bcdc3e84721a4ddfec
Jackfjs <163104280+Jackfjs@users.noreply.github.com>
2024-04-01 15:33:05 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -45,31 +45,31 @@ document.getElementById("feedbackForm").addEventListener("submit", function(even
 
 
   // Функция для проверки, если элемент находится в видимой области окна
-  function isInViewport(element) {
-    const rect = element.getBoundingClientRect();
-    return (
-        rect.top >= 0 &&
-        rect.left >= 0 &&
-        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
-        rect.right <= (window.innerWidth || document.documentElement.clientWidth)
-    );
-}
+//   function isInViewport(element) {
+//     const rect = element.getBoundingClientRect();
+//     return (
+//         rect.top >= 0 &&
+//         rect.left >= 0 &&
+//         rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+//         rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+//     );
+// }
 
 // Функция для добавления или удаления класса при прокрутке страницы
-function toggleAnimation() {
-    const sections = document.querySelectorAll('.animate');
-    sections.forEach(section => {
-        if (isInViewport(section)) {
-            section.classList.add('active');
-        } else {
-            section.classList.remove('active');
-        }
-    });
-}
+// function toggleAnimation() {
+//     const sections = document.querySelectorAll('.animate');
+//     sections.forEach(section => {
+//         if (isInViewport(section)) {
+//             section.classList.add('active');
+//         } else {
+//             section.classList.remove('active');
+//         }
+//     });
+// }
 
-document.addEventListener('scroll', toggleAnimation);
+// document.addEventListener('scroll', toggleAnimation);
 
-toggleAnimation();
+// toggleAnimation();
 
 // портфолио
 
